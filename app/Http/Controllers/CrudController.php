f08fca00bd3bba865da2517a54c0435aa607bf99
Hadziq Mtqn <hadziqwebid@gmail.com>
2020-08-24 23:04:56 +0000

    Add new file

---
+++ app/Http/Controllers/CrudController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Crud;
+use Illuminate\Http\Request;
+
+class CrudController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $title = 'Data Crud';
+        $data = Crud::latest()->paginate(5);
+  
+        return view('crud.index',compact('data','title'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+
+        // $data = Crud::latest()->get();
+        // return view('crud.index', compact('data','title'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $title = 'Menambahkan data baru';
+
+        return view('crud.create',compact('title'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'judul' => 'required',
+            'deskripsi' => 'required',
+            'photo' => 'required',
+        ]);
+
+        $data['judul'] = $request->judul;
+        $data['deskripsi'] = $request->deskripsi;
+        $data['created_at'] = date('Y-m-d H:i:s');
+        $data['updated_at'] = date('Y-m-d H:i:s');
+
+        $file = $request->file('photo');
+        if($file){
+            $nama_file = rand().'-'. $file->getClientOriginalName();
+            $file->move('assets',$nama_file);
+            $data['photo'] = 'assets/' .$nama_file;
+        }
+
+
+        Crud::create($data);
+        return redirect()->route('crud.index')->with('sukses','Data berhasil diinput');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $title = 'Detail Crud';
+
+        $data=Crud::find($id);
+        return view('crud.show',compact('title','data'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $title = 'Edit data Crud';
+
+        $data=Crud::find($id);
+        return view('crud.edit',compact('title','data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'judul' => 'required',
+            'deskripsi' => 'required',
+        ]);
+
+        $data['judul'] = $request->judul;
+        $data['deskripsi'] = $request->deskripsi;
+        $data['updated_at'] = date('Y-m-d H:i:s');
+
+        $file = $request->file('photo');
+        if($file){
+            $nama_file = rand().'-'. $file->getClientOriginalName();
+            $file->move('assets',$nama_file);
+            $data['photo'] = 'assets/' .$nama_file;
+        }
+
+        Crud::where('id',$id)->update($data);
+        return redirect()->route('crud.index')->with('sukses','Data berhasil diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data=Crud::find($id);
+        $data->delete();
+  
+        return redirect()->route('crud.index')
+                        ->with('sukses','Data berhasil dihapus');
+    }
+}
